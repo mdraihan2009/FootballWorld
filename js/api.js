b2bce4e1f8daf2131dfc1fd45834146424e749d2
Md Raihan <md.raihan.35k@gmail.com>
2026-07-28 16:43:16 +0600

    Update API key

--- js/api.js
+++ js/api.js
@@ -3,9 +3,9 @@
 // ===============================
 
 // API-Football (Live, Teams, Players, Schedule)
-"31224eabdb76846c1329b02f19e3131d";
+const API_KEY = "6ba594fc4e42108183c4d05c052b9dc0";
 const API_URL = "https://v3.football.api-sports.io";
 
 // GNews API (Football News)
-const GNEWS_API_KEY "8fbf97457cf8e15563768fa237e8b663";
+const GNEWS_API_KEY = "8fbf97457cf8e15563768fa237e8b663";
 const GNEWS_API_URL = "https://gnews.io/api/v4";
